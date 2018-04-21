63c0c0c49de3383a6b759db96be07ee30dd656af
pdvega <pdvegaa@gmail.com>
2018-04-21 15:31:43 +0700

    expo init, setup confirm order page

---
+++ youex/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Pageone from './src/component/Pageone'
+
+export default class App extends Component {
+  render() {
+    return (
+      <Pageone />
+    );
+  }
+}
